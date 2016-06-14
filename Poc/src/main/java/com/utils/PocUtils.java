64f75eb9cad1d73fc7e5423df6ee94f53a67c491
sayee <sayeeprasath.neelamegam@sky.uk>
2016-06-14 16:30:08 +0100

    Initial commmit

---
+++ Poc/src/main/java/com/utils/PocUtils.java
@@ -0,0 +1,13 @@
+package com.utils;
+
+public class PocUtils {
+	
+	public static void printVersion() {
+		System.out.println("Version 0.1");
+	}
+	
+	public static void main(String[] args) {
+		printVersion();
+	}
+
+}
