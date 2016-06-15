5edcafbfc025f379a8a9e9c8e8c2ffa59c77075b
sayee <sayeeprasath.neelamegam@sky.uk>
2016-06-15 12:50:32 +0100

    Util from Poc project

---
+++ PocUtils/src/com/utils/PocUtils.java
@@ -0,0 +1,14 @@
+package com.utils;
+
+public class PocUtils {
+	
+	public static void printVersion() {
+		System.out.println("Version 0.1");
+	}
+	
+	public static void main(String[] args) {
+		printVersion();
+		System.out.println("End");
+	}
+
+}
